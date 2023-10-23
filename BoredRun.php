7c54b908f15dbf07deff46413caebe2a7a9c37de
jaiel666 <mgusts23@gmail.com>
2023-10-23 18:50:05 +0300

    first commit

--- BoredRun.php
+++ BoredRun.php
@@ -2,7 +2,16 @@
 
 require 'vendor/autoload.php';
 
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use App\BoredApplication;
+
+$output = new ConsoleOutput();
+
+$outputStyle = new OutputFormatterStyle('black', 'white');
+$output->getFormatter()->setStyle('fire', $outputStyle);
+
+$output->writeln('<fire>----Random activity list-----</>');
 
 $app = new BoredApplication();
 $app->run();
--- boredclasses/BoredApplication.php
+++ boredclasses/BoredApplication.php
@@ -6,15 +6,17 @@ class BoredApplication
 {
     public function run(): void
     {
-        $api = new BoredAPI();
-        $activityData = $api->fetchRandomActivity();
+        for ($i = 1; $i <= 5; $i++) {
+            $api = new BoredAPI();
+            $activityData = $api->fetchRandomActivity();
 
-        if (isset($activityData['activity'])) {
-            $activityName = $activityData['activity'];
-            $activity = new Bored($activityName);
-            echo 'Random Activity: ' . $activity->getName() . "\n";
-        } else {
-            echo 'Failed to fetch a random activity.' . "\n";
+            if (isset($activityData['activity'])) {
+                $activityName = $activityData['activity'];
+                $activity = new Bored($activityName);
+                echo 'Random Activity ' . $i . ': ' . $activity->getName() . "\n";
+            } else {
+                echo 'Failed to fetch a random activity.' . "\n";
+            }
         }
     }
 }
